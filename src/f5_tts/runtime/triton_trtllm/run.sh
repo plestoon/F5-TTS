38c21ed8fb18cdf20b690f93875d5796a829477a
Don Han <contact@handong.dev>
2026-03-13 14:37:13 +0800

    Update run.sh

--- src/f5_tts/runtime/triton_trtllm/run.sh
+++ src/f5_tts/runtime/triton_trtllm/run.sh
@@ -5,7 +5,7 @@ if [ -z "$model" ]; then
     model=F5TTS_v1_Base
 fi
 echo "Start stage: $stage, Stop stage: $stop_stage, Model: $model"
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9
 
 CKPT_DIR=../../../../ckpts
 TRTLLM_CKPT_DIR=$CKPT_DIR/$model/trtllm_ckpt
